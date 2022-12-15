447926244f0c8a91194da2502d35d9596559700a
yusiangeng <engyusiang@icloud.com>
2022-12-15 18:57:49 +0800

    Show number of connected clients

--- server.ts
+++ server.ts
@@ -3,6 +3,8 @@ import { WebSocket, WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 8081 });
 
 wss.on("connection", function connection(ws) {
+  console.log("connection event. clients:", wss.clients.size);
+
   ws.on("message", function message(data, isBinary) {
     console.log("received: %s", data);
     wss.clients.forEach(function each(client) {
